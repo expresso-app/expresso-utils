409317ef1b3ae391146d3b55cf69007a2b2fc767
khattab88 <m_khatab_88@hotmail.com>
2020-09-23 20:48:31 +0200

    update test file again

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const res = helpers.filterObject({ id: 1, name: "ali", age: 30 }, "name", "age")
 console.log(res);
 
 //utils.helpers.echo();
-helpers.echo("khattab");
+helpers.echo("expresso app");
 
 //console.log(catchAsync);
 
